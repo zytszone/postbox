1d7cbfed73071df1597381436dabb67259694c33
wangyong-king <421129686@qq.com>
2017-06-20 09:29:38 +0800

    #公众号

--- postbox-web/src/main/java/cn/datai/gift/web/controller/IndexController.java
+++ postbox-web/src/main/java/cn/datai/gift/web/controller/IndexController.java
@@ -6,6 +6,7 @@ import cn.datai.gift.persist.po.UserWxInfo;
 import cn.datai.gift.utils.RespResult;
 import cn.datai.gift.utils.enums.RespCode;
 import cn.datai.gift.web.plugin.annotation.Auth;
+import cn.datai.gift.web.plugin.vo.MobileCode;
 import cn.datai.gift.web.plugin.vo.UserLoginInfo;
 import cn.datai.gift.web.service.BaseInfoService;
 import cn.datai.gift.web.service.SmsSenderService;
@@ -18,7 +19,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Random;
@@ -57,13 +57,21 @@ public class IndexController extends BaseController{
     @RequestMapping(value = "/bind",method = RequestMethod.POST)
     @ResponseBody
     public RespResult bind(@RequestParam("phone") String phone,
-                             @RequestParam("isSpecial") String isSpecial,
-                             @ModelAttribute("userWxInfo")UserWxInfo userWxInfo,
-                             @ModelAttribute("userLoginInfo")UserLoginInfo userLoginInfo, @RequestParam("redirecturl") String redirecturl,HttpServletRequest request){
+                           @RequestParam("isSpecial") String isSpecial,
+                           @ModelAttribute("userWxInfo")UserWxInfo userWxInfo,
+                           @ModelAttribute("userLoginInfo")UserLoginInfo userLoginInfo,
+                           @RequestParam("redirecturl") String redirecturl,
+                           @RequestParam("code") String code,
+                           HttpServletRequest request){
         try {
             RespResult respResult = checkParams(phone, isSpecial);
             if(!respResult.getCode().equals(RespCode.SUCCESS.getCode())){
                 return new RespResult(RespCode.FAIL,"注册参数错误");
+            }
+            //判断验证是否正确是不是超时
+            MobileCode mobileCode = userLoginInfo.getMobileCode();
+            if(!checkCode(mobileCode,phone,code)){
+                return new RespResult(RespCode.FAIL,"手机号或验证码错误");
             }
 
             //绑定手机号操作
@@ -107,15 +115,24 @@ public class IndexController extends BaseController{
      */
     @RequestMapping(value = "/smsSend",method = RequestMethod.POST)
     @Auth(needLogin = true,weixinJsAuth = true)
-    public RespResult smsSend(String mobile,@ModelAttribute("customerInfoId") Long customerInfoId){
+    public RespResult smsSend(String mobile,@ModelAttribute("userLoginInfo") UserLoginInfo userLoginInfo){
         try {
             if(!isMobileNO(mobile)){
                 return new RespResult(RespCode.FAIL,"手机号输入错误");
             }
-            String code = getRandomCode();
-            boolean result = smsSenderService.sendCaptcha(mobile,code);
+            String code = getRandomCode();//验证码
+            boolean result = smsSenderService.sendCaptcha(mobile,code);//发送短信
 
-            return new RespResult(RespCode.SUCCESS,"发送成功");
+            if(result){
+                MobileCode mobileCode = new MobileCode();
+                mobileCode.setMobile(mobile);//手机号
+                mobileCode.setCode(code);//验证码
+                mobileCode.setDateTime(System.currentTimeMillis() + 2 * 60 * 1000);//设置为两分钟超时
+                userLoginInfo.setMobileCode(mobileCode);//存放session中
+                return new RespResult(RespCode.SUCCESS,"发送成功");
+            }else{
+                return new RespResult(RespCode.FAIL,"短信发送失败");
+            }
         } catch (Exception e) {
             e.printStackTrace();
             logger.error("发送短信失败，错误信息：{}",mobile);
@@ -187,6 +204,30 @@ public class IndexController extends BaseController{
             sb.append(ch);
         }
         return sb.toString();
+    }
+
+    /**
+     * 校验手机验证码是否正确是否超时
+     * @param mobileCode
+     * @param code
+     * @return
+     */
+    private static boolean checkCode(MobileCode mobileCode,String mobile,String code){
+        if(null == mobileCode || StringUtils.isEmpty(code) || StringUtils.isEmpty(mobile)){
+            return false;
+        }
+        if(!mobileCode.getCode().equals(code)){
+            return false;
+        }
+        if(!mobileCode.getMobile().equals(mobile)){
+            return false;
+        }
+        Long periodTime = System.currentTimeMillis() - mobileCode.getDateTime();
+        if(periodTime > 2 * 60 * 1000){
+            return false;
+        }
+
+        return true;
     }
 
 
