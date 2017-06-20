cc489dc682bda42c69a761126c491247680f8201
hanzhdy@163.com <xialongxiang@jsdttec.com>
2017-06-20 17:30:13 +0800

    待领包裹

--- postbox-web/src/main/java/cn/datai/gift/web/controller/CustomerInfoController.java
+++ postbox-web/src/main/java/cn/datai/gift/web/controller/CustomerInfoController.java
@@ -1,9 +1,19 @@
 package cn.datai.gift.web.controller;
 
+import cn.datai.gift.persist.po.TBoxInfo;
+import cn.datai.gift.persist.po.TCustomerInfo;
+import cn.datai.gift.web.plugin.vo.UserLoginInfo;
 import cn.datai.gift.web.service.BoxInfoService;
+import cn.datai.gift.web.service.CustomerInfoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * 客户信息管理
@@ -13,5 +23,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("customer/")
 public class CustomerInfoController extends BaseController {
     @Autowired
-    private BoxInfoService boxInfoService;
+    private BoxInfoService      boxInfoService;
+
+    @Autowired
+    private CustomerInfoService customerInfoService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerInfoController.class);
+
+    @RequestMapping("toReceivingList")
+    public String toReceivingList(Model model) {
+        return "/postbox/toReceivingList";
+    }
+
+    @RequestMapping("getReceivingList")
+    public String getReceivingList(Model model, @ModelAttribute("userLoginInfo") UserLoginInfo userLoginInfo) {
+        try {
+            TCustomerInfo customerInfo = this.customerInfoService.queryById(userLoginInfo.getCustomerInfoId());
+            List<TBoxInfo> dataList = this.boxInfoService.queryByMobilePhone(customerInfo.getMobilePhone());
+            model.addAttribute("dataList", dataList);
+        }
+        catch (Exception ex) {
+            logger.error("获取待领快件信息失败", ex);
+        }
+        return null;
+    }
 }
---
+++ postbox-web/src/main/webapp/WEB-INF/jsp/postbox/toReceivingList.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ include file="/WEB-INF/jsp/include/include.jsp" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>我的待领列表</title>
+</head>
+<body>
+aaa
+</body>
+</html>
